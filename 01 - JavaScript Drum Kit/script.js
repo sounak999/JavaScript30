15ec0b52947cfd59f6ead0f0ba03469f8bab3cbc
Sounak Saha <sounaksaha999@gamil.com>
2024-02-21 01:48:31 +0530

    Day 1 Challenge code

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,25 @@
+const keys = document.querySelector('.keys');
+
+document.addEventListener('keydown', (event)=> {    
+    const dataVal = event.keyCode;
+    console.log(dataVal);
+
+    const key = keys.querySelector(`div[data-key='${dataVal}']`);
+    if (key !== null) {
+        key.classList.add('playing');
+    }
+
+    const audio = document.querySelector(`audio[data-key='${dataVal}']`);
+    if (audio !== null) {
+        // reset audio playback to start
+        audio.currentTime = 0;
+        audio.play();
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    const dataVal = event.keyCode;
+
+    const key = keys.querySelector(`div[data-key='${dataVal}']`);
+    key.classList.remove('playing');
+})
